db09073185cd39f2dfc49a4e3fec0066c5ec954f
PopanCristian <p_cristi10@yahoo.com>
2025-01-20 12:40:10 +0200

    Models

--- login_accounts/urls.py
+++ login_accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LogoutView
 
 
 
 urlpatterns = [
     path('', views.login_signup_view),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     ]
--- order_app/models.py
+++ order_app/models.py
@@ -1,3 +1,41 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+# Categories of Products
+class Category( models.Model):
+    name_cateogiry = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name_category
+
+class Customer( models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    phone = models.CharField(max_length = 10)
+    email = models.EmailField(max_length = 30)
+    password = models.CharField(max_length = 75)
+
+    def __str__(self):
+        return f"User: {self.first_name} {self.last_name} with phone number: {self.phone}"
+
+class Product( models.Model):
+    product_name = models.CharField(max_length = 50)
+    product_price = models.DecimalField(default=0, decimal_places = 2, max_digits = 5)
+    product_category = models.ForeignKey(Category, on_delete = models.CASCADE, default="None")
+    product_description = models.CharField(max_length = 300, default='', null=True)
+    product_image = models.ImageField(upload_to='images_products')
+
+    def __str__(self):
+        return f"produsul cu numele {self.product_name}"
+
+class Order( models.Model):
+    order_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    adress =models.CharField(max_length=50, blank=False)
+    phone = models.CharField(max_length=10, blank = False)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.order_product}"
